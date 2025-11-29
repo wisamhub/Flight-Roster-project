e647b3c6477a849b7973d91055d883f630b1abfa
wissam <wisamfakihosman@gmail.com>
2025-11-29 20:11:29 +0300

    added some checkers to avoid accessing pages in unintended way
    only done for the passenger but staff will have their own checkers soon

--- Backend/index.js
+++ Backend/index.js
@@ -12,6 +12,21 @@ const port = 3000
 const saltRounds = 10;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+//object of the passenger currently using the software
+var passenger = {
+    "ticketId":-1
+};
+
+//object of the staff currently using the software
+var staff = {
+    "Id":-1
+};
+
+//other members in the selected flight
+var flight = {
+    flightNumber: "none",
+};
+
 
 const password = "hello";
 //use this to get the hashing
@@ -44,41 +59,74 @@ app.get("/",(req, res)=>{
     res.render("home");
 })
 
-app.get("/guest", (req, res)=>{
-    res.render("flight_tracker");
-})
 app.get("/login", (req, res)=>{
     res.render("staff_login");
 })
-app.get("/guest/flight-view", (req, res)=>{
-    res.render("flight_view");
-})
-app.get("/guest/tabular-view", (req, res)=>{
-    res.render("tabular_view");
-})
-app.get("/guest/extended-view", (req, res)=>{
-    res.render("extended_view");
-})
 
+//start of passenger requests
+var error = false;
 
+app.get("/guest", (req, res)=>{
+    passenger = {
+    "ticketId":-1,
+    };
+
+    flight = {
+    flightNumber: "none",
+    otherPassengers: [],
+    otherStaff: []
+    };
+
+    res.render("flight_tracker", {error});
+    error = false;
+})
 
 app.post("/guest/tabular-view", (req, res)=>{
     const guestInput = req.body.flightInput.trim();
     const ticketPattern = /^\d{9}$/;        // 9 digits only
     const flightPattern = /^AN\d{3}$/i;     // starts with AN, then 3 digits
     if(ticketPattern.test(guestInput)){
+        passenger["ticketId"]=guestInput;
         //DB queries will be done here
         res.render("tabular_view");
     } else if(flightPattern.test(guestInput)){
+        flight["flightNumber"]=guestInput;
+        passenger["ticketId"]=0;
         //also here
         res.render("tabular_view");
     } else{
-        res.render("flight_tracker", {error:true});
+        error=true;
+        res.redirect("/guest");
     }
-})
+});
+
+app.get("/guest/flight-view", (req, res)=>{
+    if(passenger["ticketId"]==-1||flight["flightNumber"]==="none"){
+        res.redirect('/guest');
+    } else {
+    res.render("flight_view");
+    }
+});
+
+app.get("/guest/tabular-view", (req, res)=>{
+     if(passenger["ticketId"]==-1||flight["flightNumber"]==="none"){
+        res.redirect('/guest');
+    } else {
+    res.render("tabular_view");
+    }
+});
+
+app.get("/guest/extended-view", (req, res)=>{
+    if(passenger["ticketId"]==-1||flight["flightNumber"]==="none"){
+        res.redirect('/guest');
+    } else {
+    res.render("extended_view");
+    }
+});
+//end of passenger requests
 
 app.get("/test", (req, res)=>{
-    res.render("flight_view");
+    res.render("flight_list");
     //simple test URL will be removed later change file name to check certain pages too
 }) 
 
